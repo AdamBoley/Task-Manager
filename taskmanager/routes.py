7bd4749386d7d6a6707a371ce5a01bf716a52334
AdamBoley <adamboley92@gmail.com>
2022-07-03 17:50:55 +0000

    add code to update categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -17,7 +17,7 @@ def categories():
     # the above will query the database and return all records from the Categories table, sorting by category_name
     return render_template("categories.html", categories=categories)
     # in the return statement, the first categories is a variable name that can be used in the HTML file
-    # the seconc categories is the categories variable in the function, which holds the list
+    # the second categories is the categories variable in the function, which holds the list
 
 
 @app.route("/add_category", methods=["GET", "POST"])
@@ -31,3 +31,13 @@ def add_category():
     return render_template("add_category.html")
 
 
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
+    # in the return statement, the first categories is a variable name that can be used in the HTML file
+    # the second category is the category variable in the function, which is the category to be edited
