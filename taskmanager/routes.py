b40d869508c47fded40972998754255a749188c4
AdamBoley <adamboley92@gmail.com>
2022-07-03 14:50:32 +0000

    add base.html and Materialize

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,7 +5,7 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("tasks.html")
 
 
 # up to here is the standard way to set up a Flask application
---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // initialize mobile side navbar
+    // sideNav() is a pre-built function
+    $(".button-collapse").sideNav();
+})
+
+
